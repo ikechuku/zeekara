085a118d174b16325e2d454a04700e22e09fd6f8
sir_ik <sir_ik@yahoo.com>
2019-07-19 15:38:14 +0100

    add boilerplate

--- zeekara/settings/base.py
+++ zeekara/settings/base.py
@@ -27,7 +27,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'demo.urls'
+ROOT_URLCONF = 'zeekara.urls'
 
 TEMPLATES = [
     {
@@ -45,7 +45,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'demo.wsgi.application'
+WSGI_APPLICATION = 'zeekara.wsgi.application'
 
 LANGUAGE_CODE = 'en-us'
 TIME_ZONE = 'UTC'
