e9cbed7629796518ca627079aa1432d63c1e39ee
sir_ik <sir_ik@yahoo.com>
2019-07-20 02:01:23 +0100

    Configure views for home, product and checkout

--- core/views.py
+++ core/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .models import Item
 
-# Create your views here.
+def homepage(request):
+    context = {
+        'items': Item.objects.all()
+    }
+    return render(request, 'home-page.html', context)
+
+
+def checkout(check)
