d58641e6980f1a1f925590c192e9e2bc42191ec9
sir_ik <sir_ik@yahoo.com>
2019-07-25 23:49:18 +0100

    Add add_to_cart to views.py

--- core/views.py
+++ core/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from django.views.generic import ListView, DetailView
-from .models import Item
+from .models import Item, Order, OrderItem
 
 
 def products(request):
@@ -21,3 +21,24 @@ class HomeView(ListView):
 class ItemDetailView(DetailView):
     model = Item
     template_name = "product.html"
+
+
+def add_to_cart(request, pk):
+    item = get_object_or_404(Item, pk=pk)
+    order_item = OrderItem.objects.create(item=item)
+    order_qs = Order.objects.filter(user=request.user, ordered=False)
+
+    if order_qs.exists():
+        order = order_qs[0]
+
+        # check if the ordered item is in the order
+        if order.items.filter(item__pk=item.pk).exists():
+            order_item.quantity += 1
+            order_item.save()
+
+    else:
+        order = Order.objects.create(user=request.user)
+        order.items.add(order_item)
+
+    return redirect("core:product", kwargs={"pk": pk})
+
